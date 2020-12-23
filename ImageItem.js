756a7c32173bb54868cc8f196c2ca44cc3598ab9
Joshua Pinter <joshuapinter@gmail.com>
2020-12-23 11:50:30 -0700

    Add `accessibilityLabel` to thumbnail.
    This makes it easier to test, etc. Default the value of this to "thumbnail".
    
    TODO: Add optional prop for what the `accessibilityLabel` should be.

--- ImageItem.js
+++ ImageItem.js
@@ -62,6 +62,7 @@ class ImageItem extends Component {
       <TouchableOpacity
         style={{ marginBottom: imageMargin, marginRight: imageMargin }}
         onPress={() => this.handleClick(image)}
+        accessibilityLabel="thumbnail"
       >
         <Image
           source={{ uri: image.uri }}
